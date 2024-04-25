059dc2e424c583f9be412a195e9cf40771194b38
Ryan-Brosius <103217571+Ryan-Brosius@users.noreply.github.com>
2024-04-24 22:54:05 -0400

    Fixing up repo, starting to add drawing code for engine

--- openGL-Voxel-Engine/src/main.cpp
+++ openGL-Voxel-Engine/src/main.cpp
@@ -1,40 +1,68 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shader.h"
+#include "VBO.h"
+#include "EBO.h"
+#include "VAO.h"
+
+GLfloat vertices[] =
+{
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+};
 
 int main(void)
 {
-    GLFWwindow* window;
+    glfwInit();
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow(800, 800, "NAME", NULL, NULL);
+    if (window == NULL)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
     gladLoadGL();
 
-    /* Loop until the user closes the window */
+    glViewport(0, 0, 800, 800);
+
+    Shader shader("default.vert", "default.frag");
+
+    VAO VAO1;
+    VAO1.Bind();
+
+    VBO VBO1(vertices, sizeof(vertices));
+
+    VAO1.LinkVBO(VBO1, 0);
+    VAO1.Unbind();
+    VBO1.Unbind();
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
+        shader.Init();
+        VAO1.Bind();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
+    VAO1.Delete();
+    VBO1.Delete();
+    shader.Delete();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
